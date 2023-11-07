f4a9f35032cd6e75ab833dc0bc0a691cfd1e6c50
Gonzalo Alvarez <gonzalo.alvarez.atos@elo.com.br>
2023-11-07 19:34:33 -0300

    setup

---
+++ src/main/java/com/gtecno/karajan/PrincipalApp/PrincipalAppApplication.java
@@ -0,0 +1,13 @@
+package com.gtecno.karajan.PrincipalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtecno/karajan/PrincipalApp/PrincipalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecno.karajan.PrincipalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
